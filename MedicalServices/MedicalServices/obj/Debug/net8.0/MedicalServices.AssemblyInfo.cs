85b9c33178a1c6481fbdf640c4d6a3e51d94d24e
Omar Zaky <omarzaky078@gmail.com>
2025-04-13 17:24:30 +0200

    The Project Final

--- MedicalServices/MedicalServices/obj/Debug/net8.0/MedicalServices.AssemblyInfo.cs
+++ MedicalServices/MedicalServices/obj/Debug/net8.0/MedicalServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicalServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c11efecd9474867ae1d0c472ff3d6c0f7a5f304e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedicalServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedicalServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
